320fb91935bd45004331f888c8bad765957c9912
navneeta88 <mohapatra.navneeta26@gmail.com>
2026-06-19 23:23:58 +0530

    week 3 day 5 and oops answers added

---
+++ Week_3/Day_5/Q_2.cpp
@@ -0,0 +1,18 @@
+//Number of Recent Calls
+class RecentCounter {
+public:
+    queue<int> q;
+
+    RecentCounter() {
+    }
+
+    int ping(int t) {
+        q.push(t);
+
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
