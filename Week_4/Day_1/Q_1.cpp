76ae60462d23fc6ae46d5512981515ba9ea8fa5f
navneeta88 <mohapatra.navneeta26@gmail.com>
2026-06-22 22:32:00 +0530

    week 4 day 1 answers added

---
+++ Week_4/Day_1/Q_1.cpp
@@ -0,0 +1,14 @@
+//Maximum Depth of Binary Tree
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        return 1 + max(
+            maxDepth(root->left),
+            maxDepth(root->right)
+        );
+    }
+};
---
+++ Week_4/Day_1/Q_2.cpp
@@ -0,0 +1,16 @@
+Invert Binary Tree
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        swap(root->left, root->right);
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+    }
+};
